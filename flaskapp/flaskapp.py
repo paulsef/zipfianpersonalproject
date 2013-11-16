a06dde440fa05a6bb18d65f82185b7f9ca5fe2b6
paul <minton.paul@gmail.com>
2013-11-16 14:35:18 -0800

    broken python module search paths

--- flaskapp/flaskapp.py
+++ flaskapp/flaskapp.py
@@ -1,12 +1,31 @@
 import flask
 import pickle
+import pdb
 
-final_df = pickle.load(file('final_df.pkl'))
 
 app = flask.Flask(__name__)
 @app.route('/', methods = ['Get'])
 def say_hello():
 	return flask.redirect(flask.url_for('static', filename = 'landing.html'))
+
+@app.route('/slicedf', methods = ['Get', 'Post'])
+def slice_df():
+	x = flask.request.form
+	print x
+	condition1 = flask.request.form['condition1']
+	#pdb.set_trace()
+	try:
+		condition1 = int(flask.request.form['condition1'])
+	except:
+		return str(condition1) +  ' was not an integer'
+	final_df = pickle.load(file('final_df.pkl'))
+	#pdb.set_trace()
+	sliced = final_df[final_df['playcount'] > condition1]
+	return str(list(sliced['playcount']))
+
+
+
+
 
 if __name__ == '__main__':
 	app.run(debug = True)
